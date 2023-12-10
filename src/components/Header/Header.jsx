b736702c403c6eccfde2fa0cb6b301be1c6039b7
meenakshisureshbabu <meenakshi.bca@gmail.com>
2023-12-09 22:44:15 -0500

    Adding the download resume functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,18 @@ import '../Header/header.css'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const handleresumeClick = () => {
+    const pdf = "Meenakshi.pdf";
+    const link = document.createElement("a");
+    link.href = pdf;
+    link.download = "Meenakshi_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+  }
+
   return (
     <div className="header">
                 <div className="header-logo">
@@ -12,6 +24,7 @@ function Header() {
                     <div className="nav-item"><Link to="/about">About</Link></div>
                     <div className="nav-item"><Link to="/projects">Projects</Link></div>
                     <div className="nav-item"><Link to="/skills">Skills</Link></div>
+                    <div className="nav-item"><button onClick={handleresumeClick}>Download Resume</button></div>
                 </div>
         </div>
   )
