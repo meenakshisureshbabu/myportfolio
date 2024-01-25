e58b3920552ba5e095f9d857e3decd137a6a2040
meenakshisureshbabu <meenakshi.bca@gmail.com>
2024-01-25 16:49:47 -0500

    Added projects

--- src/models/projectdata.js
+++ src/models/projectdata.js
@@ -1,5 +1,56 @@
 const projdata = [
   {
+    projectname: "EXPENSE TRACKER",
+    projdescription:
+      "This expense tracker is the app where the user would be able to register and login using his credentials. All the expenses/incomes can be added and tracked. The user can view his transactions in graphical view too",
+    projimage:
+      "https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg",
+    skillsused: [
+      "https://cdn-icons-png.flaticon.com/512/919/919827.png",
+      "https://cdn-icons-png.flaticon.com/512/919/919826.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      "https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp",
+      "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
+      "https://www.svgrepo.com/show/331488/mongodb.svg",
+      "https://blog.logrocket.com/wp-content/uploads/2020/12/express-middlewares-complete-guide.png"
+
+    ],
+    // link: "https://tootyfrooty.onrender.com/",
+    link: ""
+  },
+  {
+    projectname: "TOYS INVENTORY WEBSITE",
+    projdescription:
+      "This website allows the user to add the toys to inventory(in database), delete the toys, edit the toys in the inventory. The user can search the toys also.",
+    projimage:
+      "https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg",
+    skillsused: [
+      "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
+      "https://www.svgrepo.com/show/331488/mongodb.svg",
+      "https://blog.logrocket.com/wp-content/uploads/2020/12/express-middlewares-complete-guide.png",
+      "https://cdn-icons-png.flaticon.com/512/919/919827.png",
+      "https://cdn-icons-png.flaticon.com/512/919/919826.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968292.png"
+    ],
+    // link: "https://tootyfrooty.onrender.com/",
+    link: ""
+  },
+  {
+    projectname: "DAVAM FLOWERS WEBSITE",
+    projdescription:
+      "This website displays all the products of the Davam Flowers with the pictures and price.The user would be able to contact the company via whatsapp in a single click",
+    projimage:
+      "https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg",
+    skillsused: [
+      "https://cdn-icons-png.flaticon.com/512/919/919827.png",
+      "https://cdn-icons-png.flaticon.com/512/919/919826.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      "https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp"
+    ],
+    // link: "https://tootyfrooty.onrender.com/",
+    link: "https://davamflowersjc.onrender.com/"
+  },
+  {
     projectname: "TOOTY FROOTY WEBSITE",
     projdescription:
       "This website is about the importance of fruits and nutrition about the fruits in few clicks. User will be able to spot their nearest grocery stores in map. They can also contact me for further info through a contact form.",
@@ -11,7 +62,8 @@ const projdata = [
       "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
       "https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp",
     ],
-    link: "https://tootyfrooty.onrender.com/",
+    // link: "https://tootyfrooty.onrender.com/",
+    link: ""
   },
   {
     projectname: "VANISHMAN GAME",
