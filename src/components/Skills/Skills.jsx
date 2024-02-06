8a259ed2b5a5896f5b080789ae0307cd55247b00
meenakshisureshbabu <meenakshi.bca@gmail.com>
2024-02-05 21:56:43 -0500

    Added tooltip

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,40 +6,52 @@ function Skills() {
     <div className='skill-container'>
         <div className='skill-div'>
             <div className="skill-data-div">
-            <div>
+            <div className='html'>
+                <span class="csstooltiptext">HTML 5</span>
                 <img className='skillimage' src="https://cdn-icons-png.flaticon.com/512/919/919827.png"/>
             </div>
-            <div>
+            <div className='css'>
+                <span class="csstooltiptext">CSS 3</span>
                 <img className='skillimage' src="https://cdn-icons-png.flaticon.com/512/919/919826.png"/>
             </div>
-            <div>
+            <div className='JS'>
+                <span class="csstooltiptext">Javascript</span>
                 <img className='skillimage' src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png"/>
             </div>
-            <div>
+            <div className='react'>
+                <span class="csstooltiptext">React JS</span>
                 <img className='skillimage' src="https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp"/>
             </div>
-            <div>
+            <div className='node'>
+                <span class="csstooltiptext">Node JS</span>
                 <img className='skillimage' src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"/>
             </div>
-            <div>
+            <div className='java'>
+                <span class="csstooltiptext">Java</span>
                 <img className='skillimage' src="https://cdn-icons-png.flaticon.com/512/5968/5968282.png"/>
             </div>
-            <div>
+            <div className='oracle'>
+                <span class="csstooltiptext">Oracle DB</span>
                 <img className='skillimage' src="https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/oracle-512.png"/>
             </div>
-            <div>
+            <div className='sql'>
+                <span class="csstooltiptext">SQL</span>
                 <img className='skillimage' src="https://png.pngtree.com/png-vector/20190409/ourmid/pngtree-sql-file-document-icon-png-image_920263.jpg"/>
             </div>
-            <div>
+            <div className='mongo'>
+                <span class="csstooltiptext">Mongo DB</span>
                 <img className='skillimage' src="https://www.svgrepo.com/show/331488/mongodb.svg"/>
             </div>
-            <div>
+            <div className='express'>
+                <span class="csstooltiptext">Express JS</span>
                 <img className='skillimage' src="https://blog.logrocket.com/wp-content/uploads/2020/12/express-middlewares-complete-guide.png"/>
             </div>
-            <div>
+            <div className='reacttest'>
+                <span class="csstooltiptext">React Testing Library</span>
                 <img className='skillimage' src="https://200ok.ch/img/technologies/react-testing-library.png"/>
             </div>
-            <div>
+            <div className='jest'>
+                <span class="csstooltiptext">Jest</span>
                 <img className='skillimage' src="https://miro.medium.com/v2/resize:fit:600/1*i37IyHf6vnhqWIA9osxU3w.png"/>
             </div>
             </div>
